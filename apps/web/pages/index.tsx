94f3034a097c921b5c7c12e4b68564f84d200fd5
ismail ait bella <10639660-abellaismail@users.noreply.gitlab.com>
2023-08-20 12:04:00 +0100

    chore: test vercel

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,6 +3,7 @@ import { Button, Header, RootLayout } from "ui";
 export default function LandingPage() {
   return (
     <RootLayout>
+      Test Vercel
       <Header text="Web" />
       <Button />
     </RootLayout>
