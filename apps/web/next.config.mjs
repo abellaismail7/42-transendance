099fc187e5907a7868121e5c38bfd7797cd48eaa
ismail ait bella <10639660-abellaismail@users.noreply.gitlab.com>
2023-10-08 12:06:43 +0100

    feat: run the game in react

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -10,6 +10,10 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  webpack: function (config) {
+    config.experiments = { asyncWebAssembly: true };
+    return config;
+  },
 };
 
 export default config;
