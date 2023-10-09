122c755c52bcc06ff565791f95366992eedd517a
ismail ait bella <10639660-abellaismail@users.noreply.gitlab.com>
2023-10-09 08:52:32 +0100

    chore: remove fastify

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,17 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  type NestFastifyApplication,
-  FastifyAdapter,
-} from '@nestjs/platform-fastify';
-import { fastifyApp } from './fastify/app';
 
 async function bootstrap() {
-  const adapter = new FastifyAdapter(fastifyApp);
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    adapter,
-  );
+  const app = await NestFactory.create(AppModule);
   await app.listen(4000);
 }
 bootstrap();
